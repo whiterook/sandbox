d4557c0d5deba2983897597bce529db633caa12e
Sergey <splotnikov@gmail.com>
2017-11-12 18:59:02 -0500

    Implemented inline editing

---
+++ src/Sandbox.Web/Constants/Template.cs
@@ -0,0 +1,16 @@
+﻿namespace Sandbox.Web.Constants
+{
+    public static class Template
+    {
+        public static string MetaTitle => "MetaTitle";
+        public static string MetaDescription => "MetaDescription";
+        public static string ContentHeading => "ContentHeading";
+        public static string StartDate => "Start Date";
+        public static string Duration => "Duration";
+        public static string ContentBody => "ContentBody";
+        public static string EventImage => "Evant Image";
+        public static string Highlights => "Highlights";
+
+
+    }
+}
