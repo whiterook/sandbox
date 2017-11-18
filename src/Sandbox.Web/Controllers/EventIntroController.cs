674474def6f35bed413babf75cd8250c8153c5fe
Sergey <splotnikov@gmail.com>
2017-11-18 14:58:08 -0500

    Created EventIntro component

---
+++ src/Sandbox.Web/Controllers/EventIntroController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Sandbox.Web.Models;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+
+namespace Sandbox.Web.Controllers
+{
+    public class EventIntroController : Controller
+    {
+        // GET: Events/EventIntro
+        public ActionResult Index()
+        {
+            return View(GetModel());
+        }
+
+        private EventIntro GetModel()
+        {
+            var currentItem =
+                RenderingContext.Current.ContextItem;
+
+            return new EventIntro
+            {
+                ContentBody = new HtmlString(FieldRenderer.Render(currentItem, "ContentBody")),
+                DifficultyLevel = new HtmlString(FieldRenderer.Render(currentItem, "Difficulty Level")),
+                Duration = new HtmlString(FieldRenderer.Render(currentItem, "Duration")),
+                EventImage = new HtmlString(FieldRenderer.Render(currentItem, "Event Image", "mw=400")),
+                ContentHeading = new HtmlString(FieldRenderer.Render(currentItem, "ContentHeading")),
+                Highlights = new HtmlString(FieldRenderer.Render(currentItem, "Highlights")),
+                ContentIntro = new HtmlString(FieldRenderer.Render(currentItem, "ContentIntro")),
+                StartDate = new HtmlString(FieldRenderer.Render(currentItem, "Start Date"))
+            };
+        }
+    }
+}
---
+++ src/Sandbox.Web/Models/EventIntro.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace Sandbox.Web.Models
+{
+    public class EventIntro
+    {
+        public HtmlString ContentHeading { get; set; }
+        public HtmlString ContentIntro { get; set; }
+        public HtmlString ContentBody { get; set; }
+        public HtmlString EventImage { get; set; }
+        public HtmlString Highlights { get; set; }
+        public HtmlString StartDate { get; set; }
+        public HtmlString Duration { get; set; }
+        public HtmlString DifficultyLevel { get; set; }
+
+    }
+}
