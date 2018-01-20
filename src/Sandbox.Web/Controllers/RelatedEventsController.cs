68501c4e977ea7ff3ad8ee47d4c572498ac9e86f
Sergey <splotnikov@gmail.com>
2018-01-20 10:57:27 -0500

    Missed items from prev commit

---
+++ src/Sandbox.Web/Controllers/RelatedEventsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sandbox.Web.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+
+namespace Sandbox.Web.Controllers
+{
+    public class RelatedEventsController : Controller
+    {
+        // GET: RelatedEvents
+        public ActionResult Index()
+        {
+            var renderingItem = RenderingContext.Current.Rendering.Item;
+            if (renderingItem == null)
+            {
+                return new EmptyResult();
+            }
+
+            MultilistField multiListField = renderingItem.Fields["Related Events"];
+            if (multiListField == null)
+            {
+                return new EmptyResult();
+            }
+
+            var relatedEvents =
+                multiListField.GetItems()
+                    .Select(x => new NavigationItem
+                    {
+                        Title = new HtmlString(x.DisplayName),
+                        Url = LinkManager.GetItemUrl(x)
+                    });
+
+            
+            return View(relatedEvents);
+        }
+    }
+}
---
+++ src/Sandbox.Web/Foundation/RegisterDependencies.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Sandbox.Web.Foundation
+{
+    public class RegisterDependencies : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
